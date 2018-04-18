6d471736915cfd9a313dd0c60a3f15327cec8955
Aguilar Lucas <lucasaguilar@gmail.com>
2018-04-18 15:19:45 -0300

    - Se setea color a background de container y container-fluid.  - Se agrega archivo de deploy example para automatizar tareas de compilación de scss a css para el tema de bootstrap de gobierno.

--- scripts/deploy-theme-bootstrap-example.sh
+++ scripts/deploy-theme-bootstrap-example.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#Para el tema de boostrap  de gobierno
 #automatiza la compilacion y el deploy del archivo scss a css entre proyectos utilizando node-sass
 
 #ruta para el archivo compilado
-RUTA_ORIGEN="/home/lucas/maquinaVirtualPhp5.6/www/html/bootstrap-theme/src/node_modules/mdbootstrap/scss/"
-ARCHIVO_SCSS="_custom.scss"
+RUTA_ORIGEN="/home/lucas/maquinaVirtualPhp5.6/www/html/bootstrap-theme/src/node_modules/bootstrap/scss/"
+ARCHIVO_SCSS="custom.scss"
 #ruta del proyecto destino
-RUTA_DESTINO="/home/lucas/maquinaVirtualPhp5.6/www/html/bootstrap-theme/src/dist/theme-mdbootstrap-deim-v1.0/"
-ARCHIVO_CSS="custom-mdb.css"
+RUTA_DESTINO="/home/lucas/maquinaVirtualPhp5.6/www/html/bootstrap-theme/src/dist/theme-bootstrap-deim-v1.0/"
+ARCHIVO_CSS="custom.css"
+#deploy en el bundle de temas
 RUTA_DESTINO_BUNDLE="/home/lucas/maquinaVirtualPhp5.6/www/html/bundle-stg-theme-bundle/STG/ThemeBundle/Resources/public/css/"
+
 RUTA_DESTINO_PROYECTO="/home/lucas/maquinaVirtualPhp5.6/www/html/app-with-themebundle/vendor/stgbundle/theme-bundle/STG/ThemeBundle/Resources/public/css/"
 RUTA_CACHE_PROYECTO="/home/lucas/maquinaVirtualPhp5.6/www/html/app-with-themebundle/var/cache/dev"
 
